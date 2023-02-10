74e5d7c2b7add33ae755fbb52cf56aa7b52323b2
V <vephitodasai5@gmail.com>
2023-02-10 14:58:03 +0530

    first commit

---
+++ tradecampus/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tradecampus project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tradecampus.settings')
+
+application = get_wsgi_application()
